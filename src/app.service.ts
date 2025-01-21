cefb9b5c8213a1304a1af1c6096aef63b2ae238a
MosesSupposes <mosesasamuel633@gmail.com>
2025-01-20 19:02:52 -0500

    set up templating engine (hbs) and add a couple routes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! App Service';
+  }
+
+  getHomepage() {
+    return { message: 'Hello world! Step 2' };
+  }
+
+  getAboutPage() {
+    return { message: 'About Page' };
   }
 }
